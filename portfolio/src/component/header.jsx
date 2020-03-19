b62b6102878fa6c28655b57170bc63e8da6d07ab
Yiwei Zhang <happyzyw@umich.edu>
2020-03-19 18:08:13 -0500

    wrote homepage

--- portfolio/src/component/header.jsx
+++ portfolio/src/component/header.jsx
@@ -4,13 +4,17 @@ import './header.css';
 export default class Header extends Component {
     render() {
         return(
-            <ul className="header-bar">
-                <li><a className="active" href="#home">Home</a></li>
-                <li><a href="#news">Education</a></li>
-                <li><a href="#contact">Internships</a></li>
-                <li><a href="#contact">Projects</a></li>
-                <li><a href="#about">Contact</a></li>
-            </ul>
+            <div className="header-bar">
+                <ul>
+                    <li><a className="active" href="#home">Home</a></li>
+                    <li><a href="#news">Education</a></li>
+                    <li><a href="#contact">Internships</a></li>
+                    <li><a href="#contact">Projects</a></li>
+                    <li><a href="#contact">CV</a></li>
+                    <li><a href="#about">Contact</a></li>
+                </ul>
+            </div>
+
         );
     }
 }
---
+++ portfolio/src/component/home.jsx
@@ -0,0 +1,34 @@
+import React, {Component} from "react";
+import './home.css';
+
+export default class Home extends Component {
+    render() {
+        return(
+            <div className="home">
+                <img className="image" src={ require('../profile.JPG') } />
+
+                <div className="words">
+                    Hello, My Name is Yiwei Zhang! &#128521; I am pursuing a Master of Computer Science
+                    degree at Northwestern University. <p>&nbsp;</p>
+
+                    This personal site is created by myself using React.  It's pretty fun to do
+                    self-learning of some popular tech stack! <p>&nbsp;</p>
+
+                    &#x1F60D; Happening next: I will be interning at Facebook as a software engineer
+                    for the 2020 summer!
+                </div>
+
+                <nav>
+                    <ul className="link">
+                        <li><a href="https://github.com/coolzyw" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-github"></i>
+                        </a></li>
+                        <li><a href="https://www.linkedin.com/in/dhruv-b-545b52a9/" target="_blank" rel="noopener noreferrer">
+                            {/*<i className="icon-linkedin2" />*/}
+                        </a></li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+}
